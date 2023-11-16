9c25d9de7e2fb7337963c9d88063ffa344a60d5b
Bee <143116617+beethovensmalley@users.noreply.github.com>
2023-11-16 16:15:02 -0500

    Added basic Dashboard component
    I've added a php file for the dashboard to fetch information from the events table. It is still incredibly ugly but that's just because I wanted bright colors to identify what divs I was messing with.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Dashboard from './Dashboard';
+import AuthForm from './login';
 
 function App() {
   return (
@@ -7,12 +9,11 @@ function App() {
     <header className="banner">
       <h1>GSU Poster Voting</h1>
     </header>
-
+    
     {/* Main Content */}
     <main className="content">
-      {/* Your main web app content goes here */}
-      <h2>Main Content Area</h2>
-      <p>This is where your main web app content will be displayed.</p>
+      <Dashboard />
+      <AuthForm/>
     </main>
 
     {/* Footer with Static Links */}
