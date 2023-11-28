629ba97eba9bb8c7d2df2e33421c117f0168973d
sschouest <143210371+sschouest@users.noreply.github.com>
2023-11-28 03:11:26 -0500

    Update posterForm.js (typo)

--- src/posterForm.js
+++ src/posterForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import useHistory from 'react-router-dom';
-import './posterUploadForm.css'
+import './posterForm.css'
 
 const PosterUploadForm = () => {
     const [events, setEvents] = useState([]);
