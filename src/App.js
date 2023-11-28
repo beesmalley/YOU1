f4d6f1bb83820580e50bf6097f1bd37bbde46d05
sschouest <143210371+sschouest@users.noreply.github.com>
2023-11-27 20:24:36 -0500

    Added React Router Structure to App.js
    added links for relevant pages to maintain SPA architecture

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './Home';
 import AuthForm from './sidebar';
-
+import UserDashboard from './userDashboard';
 
 function App() {
   return (
-    <div className="App">
-    {/* Banner */}
-    <header className="banner">
-      <h1>GSU Poster Voting</h1>
-    </header>
-    
-    {/* Main Content */}
-    <main className="content">
-      <Home />
-      <AuthForm/>
-    </main>
+    <Router>
+      <div className="App">
+        {/* Banner */}
+        <header className="banner">
+          <h1>GSU Poster Voting</h1>
+        </header>
+        
+        {/* Navigation Links */}
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/user-dashboard">User Dashboard</Link></li>
+            {/* Add other links as needed */}
+          </ul>
+        </nav>
 
-    {/* Footer with Static Links */}
-    <footer className="footer">
-      <div className="footer-links">
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-        <a href="/terms">Terms of Service</a>
-        <a href="/privacy">Privacy Policy</a>
+        {/* Main Content */}
+        <main className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/user-dashboard">
+              <UserDashboard />
+            </Route>
+          </Switch>
+          <AuthForm />
+        </main>
+
+        {/* Footer with Static Links */}
+        <footer className="footer">
+          <div className="footer-links">
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/terms">Terms of Service</Link>
+            <Link to="/privacy">Privacy Policy</Link>
+          </div>
+        </footer>
       </div>
-    </footer>
-  </div>
-);
+    </Router>
+  );
 }
 
 export default App;
