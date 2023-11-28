3d6bc3f60a4b7048fa2e2eb77ed9ac4fd510b3ad
sschouest <143210371+sschouest@users.noreply.github.com>
2023-11-27 23:09:42 -0500

    Update posterJudging.php
    fixed to make sure that the fetching of eventsFollowed by a judge for the purpose of finding which posters a judge should review be done on the backend, not front end

--- php/posterJudging.php
+++ php/posterJudging.php
@@ -11,7 +11,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+function getEventsFollowedByJudge($conn, $judgeId) {
+    $stmt = $conn->prepare("SELECT event_id FROM events_followed WHERE judge_id = ?");
+    $stmt->bind_param("i", $judgeId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $events = $result->fetch_all(MYSQLI_ASSOC);
+    $stmt->close();
+    
+    return array_column($events, 'event_id');
+}
+
 function assignPostersToJudges($conn, $eventIds) {
+    if (empty($eventIds)) {
+        return [];
+    }
+
     $placeholders = implode(',', array_fill(0, count($eventIds), '?'));
     $types = str_repeat('i', count($eventIds));
 
@@ -23,52 +38,25 @@ function assignPostersToJudges($conn, $eventIds) {
     $posters = $result->fetch_all(MYSQLI_ASSOC);
     $stmt->close();
 
-    //fetch all judges for the events
-    $stmt = $conn->prepare("SELECT * FROM judges WHERE event_id IN ($placeholders)");
-    $stmt->bind_param($types, ...$eventIds);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $judges = $result->fetch_all(MYSQLI_ASSOC);
-    $stmt->close();
-
-    if (empty($posters) || empty($judges)) {
-        return []; //return empty array if no posters or judges
+    if (empty($posters)) {
+        return []; //return empty array if no posters
     }
 
     $assignments = [];
-    $totalJudges = count($judges);
-    $totalPosters = count($posters);
-    $postersPerJudge = ceil($totalPosters * 2 / $totalJudges);
-
-    foreach ($judges as $judge) {
-        $judgePosters = [];
-        while (count($judgePosters) < $postersPerJudge) {
-            foreach ($posters as $poster) {
-                if (!in_array($poster, $judgePosters)) {
-                    $judgePosters[] = $poster;
-                    if (count($judgePosters) == $postersPerJudge) {
-                        break;
-                    }
-                }
+    foreach ($posters as $poster) {
+        foreach ($eventIds as $eventId) {
+            if ($poster['event_id'] == $eventId) {
+                $assignments[$eventId][] = $poster;
             }
         }
-        $assignments[$judge['id']] = $judgePosters;
     }
 
     return $assignments;
 }
 
-$eventIds = isset($_GET['eventIds']) ? explode(',', $_GET['eventIds']) : [];
 $judgeId = isset($_GET['judgeId']) ? intval($_GET['judgeId']) : 0;
-array_walk($eventIds, function(&$id){ $id = intval($id); }); //rnsure all event IDs are integers
+$followedEvents = getEventsFollowedByJudge($conn, $judgeId);
+$assignments = assignPostersToJudges($conn, $followedEvents);
 
-$assignments = assignPostersToJudges($conn, $eventIds);
-
-if ($judgeId > 0 && isset($assignments[$judgeId])) {
-    //return only the posters for the specific judge
-    echo json_encode($assignments[$judgeId]);
-} else {
-    //return the entire assignment mapping
-    echo json_encode($assignments);
-}
+echo json_encode(isset($assignments[$judgeId]) ? $assignments[$judgeId] : []);
 ?>
