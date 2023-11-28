18accdc8a03f74b5ffee6766dffb31ec90f4f8c6
sschouest <143210371+sschouest@users.noreply.github.com>
2023-11-28 03:11:04 -0500

    Update posterForm.js
    added posterForm's corresponding css file's import

--- src/posterForm.js
+++ src/posterForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import useHistory from 'react-router-dom';
-
+import './posterUploadForm.css'
 
 const PosterUploadForm = () => {
     const [events, setEvents] = useState([]);
